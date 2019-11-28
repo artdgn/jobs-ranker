a991a97efa896d3caf5063fdf2c715d938715b21
artdgn <arthurdgn@gmail.com>
2019-11-29 01:10:11 +1100

    remove copy job link to clipboard.

--- jobs_ranker/ui/webapp/static/utils.js
+++ jobs_ranker/ui/webapp/static/utils.js
@@ -6,11 +6,3 @@ function clickButtonOnEnter(buttonId) {
         }
     };
 }
-
-function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
